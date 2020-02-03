f1b67259945bb6f6e45a632de995c82c6f109ff9
romanitwork <workromanmail@gmail.com>
2020-02-03 14:54:27 +0200

    done task-1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const liList = document.querySelectorAll(".item");
+console.log(`В списке ${Array.from(liList).length} категории.`);
+
+function globalList() {
+  const liList = Array.from(document.querySelectorAll("ul#categories li.item"));
+  return console.log(
+    liList.map(key => {
+      const liArray = key.querySelectorAll("li").length;
+      return `Категория: ${
+        key.querySelector("h2").innerText
+      }, Количество элементов: ${liArray}`;
+    })
+  );
+}
+globalList();
