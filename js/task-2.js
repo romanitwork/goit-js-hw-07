7e652e4f851df8e4465050b1716ecdbfe6ed5df4
romanitwork <workromanmail@gmail.com>
2020-02-03 15:19:47 +0200

    done task-2

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+];
+
+const fragment = document.createDocumentFragment();
+const list = document.querySelector('#ingredients');
+ingredients.forEach(veggie => {
+  const veggieLi = document.createElement("li");
+  veggieLi.textContent = veggie;
+  fragment.appendChild(veggieLi);
+});
+list.appendChild(fragment);
